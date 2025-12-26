a139dc7d8c8753e34830d4be2905b633166a92bf
iblameveer <veer35414@gmail.com>
2025-12-27 00:13:03 +0530

    Update polyfill-localstorage.js

--- scripts/polyfill-localstorage.js
+++ scripts/polyfill-localstorage.js
@@ -1,32 +1,10 @@
-// Simple in-memory localStorage polyfill for Node server runtime.
-if (typeof globalThis.localStorage === "undefined" || globalThis.localStorage === null || typeof globalThis.localStorage?.getItem !== 'function') {
-  (function () {
-    const store = new Map();
-    const ls = {
-      getItem(key) {
-        const k = String(key);
-        return store.has(k) ? store.get(k) : null;
-      },
-      setItem(key, value) {
-        store.set(String(key), String(value));
-      },
-      removeItem(key) {
-        store.delete(String(key));
-      },
-      clear() {
-        store.clear();
-      },
-      key(i) {
-        return Array.from(store.keys())[i] || null;
-      },
-      get length() {
-        return store.size;
-      },
-    };
-    try {
-      globalThis.localStorage = ls;
-    } catch (err) {
-      // ignore
-    }
-  })();
+if (typeof global.localStorage === "undefined" || global.localStorage === null) {
+  global.localStorage = {
+    getItem: () => null,
+    setItem: () => {},
+    removeItem: () => {},
+    clear: () => {},
+    length: 0,
+    key: () => null,
+  };
 }
